eddf938885814eab5b0b04276ffe866a9fd15e90
E. Santiago <eduardo.augusto.santiago@gmail.com>
2023-12-13 10:45:25 +0000

    Added queue and stack

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,55 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+}
+
 export default class Queue<T> {
     public length: number;
 
-    
+    public head?: Node<T>;
+    public tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const newNode: Node<T> = {
+            value: item,
+            prev: this.tail,
+            next: undefined
+        }
 
-}
+        this.length++;
+
+        if (this.head === undefined || this.tail === undefined) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
     deque(): T | undefined {
-
-}
+        if (this.head === undefined || this.tail === undefined) {
+            return undefined;
+        } else {
+            const value: T = this.head.value;
+            if (this.head.next !== undefined) {
+                this.head = this.head.next;
+                this.head.prev = undefined;
+            } else {
+                this.head = undefined;
+                this.tail = undefined;
+            }
+            this.length--;
+            return value;
+        }
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
